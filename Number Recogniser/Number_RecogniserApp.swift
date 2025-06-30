8868d327038e93d399defb74e6f3db6126cb8f24
Rishi <rishi_j@icloud.com>
2025-06-30 07:30:22 +0100

    Initial Commit

---
+++ Number Recogniser/Number_RecogniserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Number_RecogniserApp.swift
+//  Number Recogniser
+//
+//  Created by Rishi Jansari on 30/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Number_RecogniserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
